b7cc5383f4c104dd38298aa41c7b92e1e4e9ab97
echo-lalia <108598670+echo-lalia@users.noreply.github.com>
2024-04-23 17:28:00 -0700

    Update portal_main.py

--- portal_main.py
+++ portal_main.py
@@ -1,7 +1,7 @@
 import time
 import lightsensor
 import display
-from machine import Pin, freq, PWM
+from machine import Pin, freq, PWM, Timer
 
 
 # debug tools:
@@ -25,11 +25,15 @@ DISPLAY = display.Display()
 WIDTH = DISPLAY.width
 HEIGHT = DISPLAY.height
 
+# create backlight timer
+BL_TIMER = Timer(3)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ function defs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+def timer_callback(t):
+    set_backlight_from_sensor()
 
-def set_backlight_from_sensor():
+def set_backlight_from_sensor(t=None):
     if _FORCE_MAX_LIGHT_:
         BLIGHT.duty_u16(_BLIGHT_MAX)
         return
@@ -47,13 +51,23 @@ def set_backlight_from_sensor():
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Main! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def main_loop():
     
+    
+    BL_TIMER.init(period=10, mode=Timer.PERIODIC, callback=set_backlight_from_sensor)
+    
     loop_counter = 0
     while True:
-        set_backlight_from_sensor()
+        start_time = time.ticks_ms()
         DISPLAY.v_gradient(0,0, _WIDTH, _HEIGHT, (0.06388889,0.96,1.0), (0.6083333,0.82,0.69))
+        time_diff = time.ticks_diff(time.ticks_ms(), start_time)
         
         DISPLAY.show()
-        print('done!')
+        print(f"Drawing took {round(time_diff / 1000, 2)} seconds.")
         time.sleep(1)
         break
-main_loop()
+    
+# for testing, catch exceptions to deinit timer/display
+try:
+    main_loop()
+except:
+    BL_TIMER.deinit()
+    DISPLAY.tft.deinit()
