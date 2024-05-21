18436101c56aba674233dc621315102de5476b69
echo-lalia <108598670+echo-lalia@users.noreply.github.com>
2024-05-21 00:39:15 -0700

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,8 +1,5 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-#import webrepl
-#webrepl.start()
+
 import time
 # give some time to interrupt
 time.sleep(1)
